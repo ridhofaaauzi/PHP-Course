6f7c6b166a98a96971bc614fefe2e333bb8ab43a
Ridho Fauzi Grafika <ridh21142ti@student.nurulfikri.ac.id>
2024-03-17 08:05:49 +0700

    Belajar PHP

---
+++ Belajar-PHP/01_output.php
@@ -0,0 +1,28 @@
+<?php 
+    // echo - output strings, numbers, html, etc
+    // echo 123, "Hello", 10.5;
+
+    // print - Works like echo, but can only take in a single argument
+    // print 123;
+
+    // print_r() - Print single values and arrays
+    // print_r([1,2,3]);
+
+    // var_dump() - returns more info line data type and length
+    // var_dump("hello")
+
+    // var_export() - similiar to var_dump(). outputs a string representation of a variable
+    // var_export("hello") 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan PHP</title>
+</head>
+<body>
+    <h1><?=  "Post One"; ?></h1>
+</body>
+</html>
